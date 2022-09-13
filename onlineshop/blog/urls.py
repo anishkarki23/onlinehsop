7d807f097f5aeab3ecdcd71a46448ef34ce02970
anishkarki23 <anishkarki23@gmail.com>
2022-09-13 12:52:10 +0200

    first commit

---
+++ onlineshop/blog/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+from .import views
+
+
+#setting the path for the each views created in views.py
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path("contact/", views.contact, name="contact"),
+    path("shop/", views.shop , name="shop"),
+    path("cart/", views.cart, name='cart'),
+    path("checkout/", views.checkout, name='checkout'),
+    path("update_item/", views.updateItem, name='update_item'),
+    path("process_order/", views.processOrder, name='process_order'),
+
+]
